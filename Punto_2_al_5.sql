92d98f8b1d309e0cc8f2fc3ab9309317ba1db6c8
Brandon Cadavid <brandoncadavid1@gmail.com>
2018-09-29 12:47:54 -0500

    Puntos 4 y 5

--- Punto_2_al_5.sql
+++ Punto_2_al_5.sql
@@ -31,3 +31,57 @@ GRANT DBA TO OlivaDBA;
 GRANT CONNECT TO OlivaDBA;
 
 ------------------------------------------FIN PUNTO 3---------------------------------------------------------------------------
+--------------------------------------- PUNTO 4 -----------------------------------------------------------------------------
+
+-- Creación de perfiles
+create profile clerk limit
+    PASSWORD_LIFE_TIME 40
+    SESSIONS_PER_USER 1
+    IDLE_TIME 10
+    FAILED_LOGIN_ATTEMPTS 4;
+
+create profile development limit
+    PASSWORD_LIFE_TIME 100
+    SESSIONS_PER_USER 2
+    IDLE_TIME 30
+    FAILED_LOGIN_ATTEMPTS UNLIMITED;
+-------------------------------------------- FIN PUNTO 4 --------------------------------------------------------------------
+
+--------------------------------------------- PUNTO 5 -----------------------------------------------------------------------    
+-- Creación de los usuarios
+CREATE user Teacher 
+    IDENTIFIED by teacher123 
+        default tablespace uber
+            quota unlimited on uber;
+            
+GRANT CONNECT to Teacher;
+ALTER USER Teacher PROFILE development;
+
+CREATE user Juanoliva 
+    IDENTIFIED by oliva123 
+        default tablespace uber
+            quota unlimited on uber;
+            
+GRANT CONNECT to Juanoliva;
+ALTER USER Juanoliva PROFILE development;
+
+CREATE user BrandonK 
+    IDENTIFIED by brandon123 
+        default tablespace uber
+            quota unlimited on uber;
+            
+GRANT CONNECT to BrandonK;
+ALTER USER BrandonK PROFILE clerk;
+
+
+CREATE user FelipeU 
+    IDENTIFIED by pipe123 
+        default tablespace uber
+            quota unlimited on uber;
+            
+GRANT CONNECT to FelipeU;
+ALTER USER FelipeU PROFILE clerk;
+ALTER USER FelipeU account lock;
+
+----------------------------------------------- FIN PUNTO 5 ------------------------------------------------------------------
+ 
